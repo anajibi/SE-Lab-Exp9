be02926cba8318dcc328dfa4a527f9bcdc02ce54
Ali Najibi <58134947+anajibi@users.noreply.github.com>
2023-06-02 20:55:32 +0330

    fix a minor typo

--- front-end/src/service.js
+++ front-end/src/service.js
@@ -1,7 +1,7 @@
 export function addApi(number1, number2) {
-    return fetch(`http://localhost:8080/add/${number1}/${number2}`, {method: "post"}).then(response => response.json());
+    return fetch(`http://localhost:8080/api/add/${number1}/${number2}`, {method: "post"}).then(response => response.json());
 }
 
 export function subtractApi(number1, number2) {
-    return fetch(`http://localhost:8080/subtract/${number1}/${number2}`, {method: "post"}).then(response => response.json());
+    return fetch(`http://localhost:8080/api/subtract/${number1}/${number2}`, {method: "post"}).then(response => response.json());
 }
